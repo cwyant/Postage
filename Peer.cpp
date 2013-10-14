ac09afd6e7aa568c9e16faecca3733aeb9cbd5e5
Chris Wyant <chris.wyant10@gmail.com>
2013-10-13 23:34:15 -0400

    Add basic getter and setters for Peer class, more must be added in at a later time

--- Peer.cpp
+++ Peer.cpp
@@ -13,3 +13,13 @@ QString Peer::getPeerName(){
 QString Peer::getPeerType(){
     return peerType;
 }
+
+void Peer::setPeerName(QString name){
+    if(name.length()>0) //If there is no data, don't bother updating it
+        this->peerName = name;
+}
+
+void Peer::setPeerType(QString type){
+    if (type.length()>0)
+        this->peerType = type;
+}
--- Peer.h
+++ Peer.h
@@ -7,6 +7,8 @@ class Peer {
 public:
     QString getPeerName(); //returns the name of the peer
     QString getPeerType(); //returns the type of system that the peer is using i.e Linux, Windows, Mac
+    void setPeerName(QString name);
+    void setPeerType(QString type);
     Peer(); //Default constructor
 private:
     QString peerName;
